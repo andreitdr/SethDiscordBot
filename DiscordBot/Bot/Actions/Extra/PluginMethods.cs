d6398cd1ccbc58c27e3e9e6cdef580fb2210e97b
Andrei Tudor <tudor.andrei.707@gmail.com>
2024-05-21 21:00:59 +0300

    Removed usings

--- DiscordBot/Bot/Actions/Extra/PluginMethods.cs
+++ DiscordBot/Bot/Actions/Extra/PluginMethods.cs
@@ -1,14 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
 
 using DiscordBot.Utilities;
 
 using DiscordBotCore;
-using DiscordBotCore.Interfaces;
 using DiscordBotCore.Loaders;
 using DiscordBotCore.Online;
 using DiscordBotCore.Others;
