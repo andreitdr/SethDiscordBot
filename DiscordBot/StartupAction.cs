c87d1a89c5587f1398a23b2a07b5ba5abe76d19d
Andrei Tudor <tudor.andrei.707@gmail.com>
2024-05-22 11:04:14 +0300

    Moved from record to class

--- DiscordBot/StartupAction.cs
+++ DiscordBot/StartupAction.cs
@@ -1,10 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DiscordBot
 {
-    internal record StartupAction(string Command, Action<string[]> RunAction) : IStartupAction;
+    internal class StartupAction : IStartupAction
+    {
+        public string Command { get; init; }
+        public Action<string[]> RunAction { get; init; }
+
+        public StartupAction(string command, Action<string[]> runAction)
+        { 
+            this.Command = command;
+            this.RunAction = runAction;
+        }
+    }
 }
