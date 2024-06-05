bc20101795e9750aa1b92252e79f37f63294243d
Andrei Tudor <tudor.andrei.707@gmail.com>
2024-06-06 00:28:19 +0300

    Added script dependency

---
+++ DiscordBotCore/Plugin/OnlineScriptDependencyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotCore.Plugin
+{
+    public class OnlineScriptDependencyInfo
+    {
+        public string DependencyName { get; private set; }
+        public string ScriptContent { get; private set; }
+
+        public OnlineScriptDependencyInfo(string dependencyName, string scriptContent)
+        {
+            DependencyName = dependencyName;
+            ScriptContent = scriptContent;
+        }
+    }
+}
--- DiscordBotCore/Plugin/PluginOnlineInfo.cs
+++ DiscordBotCore/Plugin/PluginOnlineInfo.cs
@@ -13,19 +13,21 @@ public class PluginOnlineInfo
     public string DownLoadLink { get; private set; }
     public string Description { get; private set; }
     public List<OnlineDependencyInfo> Dependencies { get; private set; }
+    public List<OnlineScriptDependencyInfo> ScriptDependencies { get; private set; }
     public OSType SupportedOS { get; private set; }
     public bool HasDependencies { get; init; }
 
     [JsonConstructor]
-    public PluginOnlineInfo(string name, PluginVersion version, string description, string downLoadLink, OSType supportedOS, List<OnlineDependencyInfo> dependencies)
+    public PluginOnlineInfo(string name, PluginVersion version, string description, string downLoadLink, OSType supportedOS, List<OnlineDependencyInfo> dependencies, List<OnlineScriptDependencyInfo> scriptDependencies)
     {
-        Name            = name;
-        Version         = version;
-        Description     = description;
-        DownLoadLink    = downLoadLink;
-        SupportedOS     = supportedOS;
-        Dependencies    = dependencies;
+        Name = name;
+        Version = version;
+        Description = description;
+        DownLoadLink = downLoadLink;
+        SupportedOS = supportedOS;
+        Dependencies = dependencies;
         HasDependencies = dependencies.Count > 0;
+        ScriptDependencies = scriptDependencies;
     }
 
     public PluginOnlineInfo(string name, PluginVersion version, string description, string downLoadLink, OSType supportedOS)
@@ -36,6 +38,7 @@ public class PluginOnlineInfo
         DownLoadLink    = downLoadLink;
         SupportedOS     = supportedOS;
         Dependencies    = new List<OnlineDependencyInfo>();
+        ScriptDependencies = new List<OnlineScriptDependencyInfo>();
         HasDependencies = false;
     }
 
