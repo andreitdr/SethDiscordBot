0c33422b5cc4dd88022e0ec95534ad4ff870bfe0
Andrei Tudor <tudor.andrei.707@gmail.com>
2025-04-28 22:14:18 +0300

    Removed the old WebUI based on MVC

--- WebUI_Old/Models/InstalledPluginViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WebUI_OLD.Models;
-
-public class InstalledPluginViewModel
-{
-    public string Name { get; set; }
-    public string Version { get; set; }
-    public bool IsOfflineAdded { get; set; }
-}
--- WebUI_Old/Models/PluginDetailsViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebUI_OLD.Models;
-
-public class PluginDetailsViewModel
-{
-    public string PluginName { get; set; }
-    public string Version { get; set; }
-    public string Description { get; set; }
-    public string Author { get; set; }
-}
