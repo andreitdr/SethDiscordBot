fafa493db76b1a6e3b912db9446a3eaa58aea789
Andrei Tudor <tudor.andrei.707@gmail.com>
2025-05-30 18:16:53 +0300

    Added Unit tests for Logger

--- DiscordBotCore.Logging/LogMessage.cs
+++ DiscordBotCore.Logging/LogMessage.cs
@@ -1,76 +1,75 @@
-﻿namespace DiscordBotCore.Logging
+﻿namespace DiscordBotCore.Logging;
+
+internal sealed class LogMessage : ILogMessage
 {
-    internal sealed class LogMessage : ILogMessage
+    private static readonly string _DefaultLogMessageSender = "\b";
+    public string Message { get; set; }
+    public DateTime ThrowTime { get; set; }
+    public string SenderName { get; set; }
+    public LogType LogMessageType { get; set; }
+
+    public LogMessage(string message, LogType logMessageType)
     {
-        private static readonly string _DefaultLogMessageSender = "\b";
-        public string Message { get; set; }
-        public DateTime ThrowTime { get; set; }
-        public string SenderName { get; set; }
-        public LogType LogMessageType { get; set; }
+        Message = message;
+        LogMessageType = logMessageType;
+        ThrowTime = DateTime.Now;
+        SenderName = string.Empty;
+    }
 
-        public LogMessage(string message, LogType logMessageType)
-        {
-            Message = message;
-            LogMessageType = logMessageType;
-            ThrowTime = DateTime.Now;
-            SenderName = string.Empty;
-        }
+    public LogMessage(string message, object sender)
+    {
+        Message = message;
+        SenderName = sender is string && sender as string == string.Empty ? _DefaultLogMessageSender : sender.GetType().FullName ?? sender.GetType().Name;
+        ThrowTime = DateTime.Now;
+        LogMessageType = LogType.Info;
+    }
 
-        public LogMessage(string message, object sender)
-        {
-            Message = message;
-            SenderName = sender is string && sender as string == string.Empty ? _DefaultLogMessageSender : sender.GetType().FullName ?? sender.GetType().Name;
-            ThrowTime = DateTime.Now;
-            LogMessageType = LogType.Info;
-        }
+    public LogMessage(string message, object sender, DateTime throwTime)
+    {
+        Message = message;
+        SenderName = sender is string && sender as string == string.Empty ? _DefaultLogMessageSender : sender.GetType().FullName ?? sender.GetType().Name;
+        ThrowTime = throwTime;
+        LogMessageType = LogType.Info;
+    }
 
-        public LogMessage(string message, object sender, DateTime throwTime)
-        {
-            Message = message;
-            SenderName = sender is string && sender as string == string.Empty ? _DefaultLogMessageSender : sender.GetType().FullName ?? sender.GetType().Name;
-            ThrowTime = throwTime;
-            LogMessageType = LogType.Info;
-        }
+    public LogMessage(string message, object sender, LogType logMessageType)
+    {
+        Message = message;
+        SenderName = sender is string && sender as string == string.Empty ? _DefaultLogMessageSender : sender.GetType().FullName ?? sender.GetType().Name;
+        ThrowTime = DateTime.Now;
+        LogMessageType = logMessageType;
 
-        public LogMessage(string message, object sender, LogType logMessageType)
-        {
-            Message = message;
-            SenderName = sender is string && sender as string == string.Empty ? _DefaultLogMessageSender : sender.GetType().FullName ?? sender.GetType().Name;
-            ThrowTime = DateTime.Now;
-            LogMessageType = logMessageType;
+    }
 
-        }
+    public LogMessage(string message, DateTime throwTime, object sender, LogType logMessageType)
+    {
+        Message = message;
+        ThrowTime = throwTime;
+        SenderName = sender is string && sender as string == string.Empty ? _DefaultLogMessageSender : sender.GetType().FullName ?? sender.GetType().Name;
+        LogMessageType = logMessageType;
+    }
 
-        public LogMessage(string message, DateTime throwTime, object sender, LogType logMessageType)
-        {
-            Message = message;
-            ThrowTime = throwTime;
-            SenderName = sender is string && sender as string == string.Empty ? _DefaultLogMessageSender : sender.GetType().FullName ?? sender.GetType().Name;
-            LogMessageType = logMessageType;
-        }
+    public LogMessage WithMessage(string message)
+    {
+        this.Message = message;
+        return this;
+    }
 
-        public LogMessage WithMessage(string message)
-        {
-            this.Message = message;
-            return this;
-        }
+    public LogMessage WithCurrentThrowTime()
+    {
+        this.ThrowTime = DateTime.Now;
+        return this;
+    }
 
-        public LogMessage WithCurrentThrowTime()
-        {
-            this.ThrowTime = DateTime.Now;
-            return this;
-        }
+    public LogMessage WithMessageType(LogType logType)
+    {
+        this.LogMessageType = logType;
+        return this;
+    }
 
-        public LogMessage WithMessageType(LogType logType)
-        {
-            this.LogMessageType = logType;
-            return this;
-        }
-
-        public static LogMessage CreateFromException(Exception exception, object Sender, bool logFullStack)
-        {
-            LogMessage message = new LogMessage(logFullStack? exception.ToString() : exception.Message, Sender, LogType.Error);
-            return message;
-        }
+    public static LogMessage CreateFromException(Exception exception, object Sender, bool logFullStack)
+    {
+        LogMessage message = new LogMessage(logFullStack? exception.ToString() : exception.Message, Sender, LogType.Error);
+        return message;
     }
 }
